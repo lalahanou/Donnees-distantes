b7af3c1542eb3f7366e1ce4ff5fcd93a12feda44
Hana <bahloul.hana@gmail.com>
2023-07-19 13:51:58 +0100

    Ajouter une méthode qui mettera 7s à s'executer

---
+++ app/src/main/java/com/example/freezapplication/Utils/Utils.java
@@ -0,0 +1,17 @@
+package com.example.freezapplication.Utils;
+
+import android.util.Log;
+
+public class Utils {
+    public static Long executeLongActionDuring7seconds(){
+
+        Log.e("TAG", "Long action is starting...");
+        Long endTime = System.currentTimeMillis() + 7000;
+        while (System.currentTimeMillis() <  endTime) {
+            //Loop during 7 secs hehehe...
+        }
+        Log.e("TAG", "Long action is finished !");
+
+        return endTime;
+    }
+}
