2482de4ff20d37d7bed1b32c99946859c48c2881
Hana <bahloul.hana@gmail.com>
2023-07-19 15:18:46 +0100

    Manage rotation during Asyntasck using Loaders

--- app/src/main/java/com/example/freezapplication/MainActivity.java
+++ app/src/main/java/com/example/freezapplication/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.freezapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.freezapplication.Utils.MyAsynckTask;
+import com.example.freezapplication.Utils.MyAsyntasckLoader;
 import com.example.freezapplication.Utils.MyHandlerThread;
 import com.example.freezapplication.Utils.Utils;
 
-public class MainActivity extends AppCompatActivity implements MyAsynckTask.Listeners {
+public class MainActivity extends AppCompatActivity implements MyAsynckTask.Listeners, LoaderManager.LoaderCallbacks<Long> {
 
     //FOR DESIGN
     private ProgressBar progressBar;
@@ -20,6 +26,9 @@ public class MainActivity extends AppCompatActivity implements MyAsynckTask.List
     //A1- Declaring a handlerThread
     private MyHandlerThread handlerThread;
 
+    //C3- Create static ID will identify our loader
+    private static int TASK_ID = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +36,10 @@ public class MainActivity extends AppCompatActivity implements MyAsynckTask.List
 
         //Get progressbar from layout
         this.progressBar = findViewById(R.id.activity_main_progress_bar);
-        //A3Configure Handler Thread
+        //A3- Configure Handler Thread
         this.configureHandlerThread();
+        //Try to resume possible loading AsyncTask
+        this.resumeAsyncTaskLoaderIfPossible();
     }
 
     @Override
@@ -52,6 +63,9 @@ public class MainActivity extends AppCompatActivity implements MyAsynckTask.List
                 break;
             case 60: // CASE USER CLICKED ON BUTTON "EXECUTE ASYNCTASK"
                 this.startAsyncTask();
+                break;
+            case 70: // CASE USER CLICKED ON BUTTON "EXECUTE ASYNCTASKLOADER"
+                this.startAsyncTaskLoader();
                 break;
         }
     }
@@ -108,5 +122,40 @@ public class MainActivity extends AppCompatActivity implements MyAsynckTask.List
     public void updateUIAfterTask(Long taskEnd){
         progressBar.setVisibility(View.GONE);
         Toast.makeText(this, "Task is finally finished at : "+taskEnd+" !", Toast.LENGTH_SHORT).show();
+    }
+
+
+    //C- Implements  callbacks methods
+    @NonNull
+    @Override
+    public Loader onCreateLoader(int id, @Nullable Bundle args) {
+        Log.d("TAG", "On Create !");
+        this.updateUIBeforeTask();
+        //C5- Return new AsynckTaskLoader
+        return new MyAsyntasckLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader loader, Long data) {
+        Log.d("TAG", "On Finished !");
+        loader.stopLoading();  //C6- Force stop loader to stop
+        updateUIAfterTask(data);
+    }
+
+    //C7- Resume previous AsynctTaskLoader if still running
+    private void resumeAsyncTaskLoaderIfPossible(){
+        if (getSupportLoaderManager().getLoader(TASK_ID) != null && getSupportLoaderManager().getLoader(TASK_ID).isStarted()) {
+            getSupportLoaderManager().initLoader(TASK_ID, null, this);
+            this.updateUIBeforeTask();
+        }
+    }
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+
+    }
+
+    //C4- Start a new AsynchTaskLoader
+    private void startAsyncTaskLoader(){
+        getSupportLoaderManager().restartLoader(TASK_ID, null, this);
     }
 }
