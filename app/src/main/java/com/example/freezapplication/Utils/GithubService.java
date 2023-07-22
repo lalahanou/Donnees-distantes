fbd705b1b3cbc2e47e29c3988d22489eee7b1a8e
Hana <bahloul.hana@gmail.com>
2023-07-22 20:51:06 +0100

    Configure retrofit and create our first endpoint

---
+++ app/src/main/java/com/example/freezapplication/Utils/GithubService.java
@@ -0,0 +1,23 @@
+package com.example.freezapplication.Utils;
+
+import com.example.freezapplication.Models.GithubUser;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GithubService {
+
+    @GET("users/{username}/following")
+    Call<List<GithubUser>> getFollowing(@Path("username") String username);
+
+    public static final Retrofit retrofit = new Retrofit.Builder()
+            //Permet de definir à retrofit un URL racine utilisé par la suite comme URL de base
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
